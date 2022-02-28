11b79fef1d86d1f2d7b48541712ed3cd8a3799a9
hermani456 <hermani457@gmail.com>
2022-02-28 19:45:34 -0300

    eliminadas constantes y modificada la funcion escribirdom

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,76 +4,58 @@ const heroInput = document.querySelector("#hero-input")
 const heroInfo = document.querySelector("#hero-info")
 const heroStats = document.querySelector("#hero-stats")
 
+// variable con la url del api
+const URL = "https://www.superheroapi.com/api.php/4905856019427443/"
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault()
 	const valueInput = heroInput.value
 	// regex que chequea solo numeros positivos
 	const patron = /^\d*[1-9]\d*$/
-	if (!valueInput.match(patron) || valueInput > 732) {
+	// if que chequea que el numero ingresado este entre 1 y 732
+	if (valueInput > 732 || (!valueInput.match(patron))) {
 		alert("Solo se permiten numeros del rango: ( 1 - 732 )")
 		return
 	}
-	fetch(
-		"https://www.superheroapi.com/api.php/4905856019427443/" + valueInput
-	).then((respuesta) => {
+	fetch(URL + valueInput).then((respuesta) => {
 		respuesta.json().then((data) => {
-			// asignando variables a los datos entregados por el json
-			const name = data.name
-			const image = data.image.url
-			let connections = data.connections["group-affiliation"]
-			let publisher = data.biography.publisher
-			let occupation = data.work.occupation
-			let firstAppearance = data.biography["first-appearance"]
-			let height = data.appearance.height
-			let weight = data.appearance.weight
-			let aliases = data.biography.aliases
-			aliases = Object.values(aliases)
+			// asignando variable a los datos entregados por json
 			const powers = data.powerstats
 
-			// llena con unkown los datos que no arrojen resultado (-)
-			if (connections === "-") {
-				connections = "Unkown"
+			// llena con Unknown los datos que arrojen valor "-"
+			if (data.connections["group-affiliation"] === "-") {
+				data.connections["group-affiliation"] = "Unknown"
 			}
-			if (publisher === "-") {
-				publisher = "Unkown"
+			if (data.biography.publisher === "-") {
+				data.biography.publisher = "Unknown"
 			}
-			if (occupation === "-") {
-				occupation = "Unkown"
+			if (data.work.occupation === "-") {
+				data.work.occupation = "Unknown"
 			}
-			if (firstAppearance === "-") {
-				firstAppearance = "Unkown"
+			if (data.biography["first-appearance"] === "-") {
+				data.biography["first-appearance"] = "Unknown"
 			}
-			if (height["0"] === "-") {
-				height = "Unkown"
+			if (data.appearance.height["0"] === "-") {
+				data.appearance.height = "Unknown"
 			}
-			if (weight["0"] === "- lb") {
-				weight = "Unkown"
+			if (data.appearance.weight["0"] === "- lb") {
+				data.appearance.weight = "Unknown"
 			}
-
+			if (data.biography.aliases["0"] === "-") {
+				data.biography.aliases = "Unknown"
+			}
+			
 			// llamando a la funcion que escribe los datos al dom 
-			escribirDom(
-				heroInfo,
-				image,
-				name,
-				connections,
-				publisher,
-				occupation,
-				firstAppearance,
-				height,
-				weight,
-				aliases
-			)
-			// convierte los datos entregados por la variable powers para poder ser usados directamente en la grafica de canvasjs
-			// tambien convierte la primera letra de el valor de los labels a mayuscula
-			const graph = Object.entries(powers).map((e) => {
-				const label = capitalizeFirstLetter(e[0])
-				const y = e[1] * 1
-				return {
-					y,
-					label,
-				}
-			})
+			escribirDom(heroInfo,data)
 
+			// convierte los datos entregados por la variable powers para poder ser usados directamente en la grafica de canvasjs
+			// tambien convierte la primera letra de el valor de los labels a mayuscula y guarda todo en la variable graph
+			const graph = Object.entries(powers).filter(power => power[1] !== 'null').map(power => {
+				const label = capitalizeFirstLetter(power[0])
+				const y = power[1] * 1
+				return {y,label}
+			})
+		
 			// llamando a la funcion que crea el grafico pasando los parametros graph y powers
 			canvas(graph,powers)
 		})
@@ -82,25 +64,25 @@ form.addEventListener("submit", (e) => {
 // Funciones:
 
 // funcion para escribir en el dom
-function escribirDom(vari, img, name, con, pub, ocu, fa, he, we, ali) {
+function escribirDom(vari, dato) {
 	vari.innerHTML = `
 			<div class="mb-3 card" id="card">
 				<div class="row g-0">
 					<div class="col-md-4">
-						<img src="${img}" class="img-fluid rounded-start" id="img">
+						<img src="${dato.image.url}" class="img-fluid rounded-start" id="img">
 					</div>
 					<div class="col-md-8">
 						<div class="card-body">
-							<h5 class="card-title"><span class="fw-bold">Name:</span> ${name}</h5>
-							<p class="card-text"><span class="fw-bold">Connections:</span> ${con}</p>
+							<h5 class="card-title"><span class="fw-bold">Name:</span> ${dato.name}</h5>
+							<p class="card-text"><span class="fw-bold">Connections:</span> ${dato.connections["group-affiliation"]}</p>
 							<p class="card-text">
-								<small class="text-muted"><span class="fw-bold">Publisher: ${pub}</span></small>
+								<small class="text-muted"><span class="fw-bold">Publisher: ${dato.biography.publisher}</span></small>
 							</p>
-							<p class="card-text"><span class="fw-bold">Occupation:</span> ${ocu}</p>
-							<p class="card-text"><span class="fw-bold">First Appearance:</span> ${fa}</p>
-							<p class="card-text"><span class="fw-bold">Height:</span> ${he}</p>
-							<p class="card-text"><span class="fw-bold">Weight:</span> ${we}</p>
-							<p class="card-text"><span class="fw-bold">Aliases:</span> ${ali}</p>
+							<p class="card-text"><span class="fw-bold">Occupation:</span> ${dato.work.occupation}</p>
+							<p class="card-text"><span class="fw-bold">First Appearance:</span> ${dato.biography["first-appearance"]}</p>
+							<p class="card-text"><span class="fw-bold">Height:</span> ${dato.appearance.height}</p>
+							<p class="card-text"><span class="fw-bold">Weight:</span> ${dato.appearance.weight}</p>
+							<p class="card-text"><span class="fw-bold">Aliases:</span> ${dato.biography.aliases}</p>
 						</div>
 					</div>
 				</div>
@@ -133,7 +115,7 @@ function canvas(e1,e2){
 			},
 		],
 	}
-	if (e2.strength !== "null") {
+	if (e2.strength  !== "null") {
 		const chart = new CanvasJS.Chart("hero-stats", config)
 		chart.render()
 	} else {
